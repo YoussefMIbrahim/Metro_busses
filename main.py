00eb874235bc71bcc7d5c28f300141c97c84be1c
Youssef Ibrahim <youssefibrahim909@gmail.com>
2022-05-14 16:27:38 -0500

    adding comments and fir goals

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from gettext import translation
 import requests
 
+# This is the prototype applicaiton that i am making to replace the basically garbage metro
+# transit app tracking which bombards you with ads and only lets you track one 
+# bus at a time
+
+#todo remember how to make a class
+#todo make a file that deals with the API
